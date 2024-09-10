ad79766d1b1b1e6331d9d7d7de2eecc832f1b7ee
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-09-11 01:26:03 +0530

    complete backend code!

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -10,7 +10,4 @@ router.route("/get").get(isAuthenticated, getAllJobs)
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs) 
 router.route("/get/:id").get(isAuthenticated, getJobById) 
 
-
-
-
 export default router;
