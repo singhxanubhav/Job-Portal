5a19edb2bd8dfa50d92a1d7f1898fc56b3216198
Anubhav Singh <anubhavsingbkj@gmail.com>
2024-09-11 01:05:48 +0530

    Complet Backend Code!

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,6 +1,6 @@
 import { Company } from "../models/company.model.js";
-import getDataUri from "../utils/datauri.js";
-import cloudinary from "../utils/cloudinary.js";
+// import getDataUri from "../utils/datauri.js";
+// import cloudinary from "../utils/cloudinary.js";
 
 export const registerCompany = async (req, res) => {
     try {
@@ -73,13 +73,13 @@ export const updateCompany = async (req, res) => {
     try {
         const { name, description, website, location } = req.body;
  
-        const file = req.file;
-        // idhar cloudinary ayega
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-        const logo = cloudResponse.secure_url;
+        // const file = req.file;
+        // // idhar cloudinary ayega
+        // const fileUri = getDataUri(file);
+        // const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        // const logo = cloudResponse.secure_url;
     
-        const updateData = { name, description, website, location, logo };
+        const updateData = { name, description, website, location};
 
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
 
