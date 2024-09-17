f41034dc7b829833f85014d565a72936debf2f24
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-09-17 09:44:17 +0530

    Complete Signup/Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,42 @@
-// import Navbar from "./components/shared/Navbar";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+// import Jobs from './components/Jobs'
+// import Browse from './components/Browse'
+// import Profile from './components/Profile'
+// import JobDescription from './components/JobDescription'
+// import Companies from './components/admin/Companies'
+// import CompanyCreate from './components/admin/CompanyCreate'
+// import CompanySetup from './components/admin/CompanySetup'
+// import AdminJobs from "./components/admin/AdminJobs";
+// import PostJob from './components/admin/PostJob'
+// import Applicants from './components/admin/Applicants'
+// import ProtectedRoute from './components/admin/ProtectedRoute'
+
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <Home/>
+    element: <Home />
   },
   {
-    path: '/',
-    element: <Home/>
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
   }
+
 ])
+function App() {
 
-import { Home } from 'lucide-react';
-import React from 'react'
-import Navbar from "./components/shared/Navbar";
-
-const App = () => {
   return (
-    <>
-   <Navbar/>
-    </>
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
   )
 }
 
